49dfeb36d0836ef3479991b870c2cffe7f8b8fa9
myakhlaqi <yahya.akhlaghi@gmail.com>
2023-01-23 22:42:16 +0800

    Solution 367

---
+++ 367.valid-perfect-square.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=367 lang=python3
+#
+# [367] Valid Perfect Square
+#
+
+# @lc code=start
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        # return math.sqrt(num) == int(math.sqrt(num))
+        left = 1
+        right = num
+        while left<=right:
+            mid = (left+right)//2
+            if mid * mid == num:
+                return True
+            elif mid * mid < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False        
+# @lc code=end
+
