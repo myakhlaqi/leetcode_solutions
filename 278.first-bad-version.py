d5792f031243415aeb8762228bac79291bcf3ebb
Yahya <91276692+myakhlaqi@users.noreply.github.com>
2023-02-10 17:31:00 +0800

    Update 278.first-bad-version.py

--- 278.first-bad-version.py
+++ 278.first-bad-version.py
@@ -15,7 +15,7 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         l = 1
         r = n
-        while l <r:
+        while l < r:
             mid = (l + r) // 2
             if isBadVersion(mid):
                 r = mid
