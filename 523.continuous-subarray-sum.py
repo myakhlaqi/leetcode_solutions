bf511b102825c6e698e502f009262c412608b0f3
Yahya <91276692+myakhlaqi@users.noreply.github.com>
2023-03-10 14:26:08 +0800

    Update 523.continuous-subarray-sum.py

--- 523.continuous-subarray-sum.py
+++ 523.continuous-subarray-sum.py
@@ -9,7 +9,7 @@ class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         n= len(nums)
         i=0
-        prefix_sum=[0]*(n+1)
+        prefix_sum = [0]*(n+1)
         for i in range(n):
             prefix_sum[i+1]=prefix_sum[i]+nums[i]
         # print(prefix_sum)
